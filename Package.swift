1ac37aec2b54158dc4208675c6d4155c0d95e1ff
moriturus <moriturus@alimensir.com>
2022-06-01 04:06:54 +0900

    remove platforms option

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "StoredIn",
-    platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
-    ],
     products: [
         .library(
             name: "StoredIn",
